479ba7a6363787dab73a4aa46d898b09b08cd2a1
bcabrera19 <bryan.cabrera19@gmail.com>
2019-04-11 13:11:28 -0400

    Finished connecting backend. Now time to make it look pretty

---
+++ eStartup/js/dbconnection.php
@@ -0,0 +1,20 @@
+<?php
+
+    
+        $host = "localhost";
+        $user = "lgarcia2013";
+        $password = "UCT2m21h4q";
+        $database = "lgarcia2013";
+        
+        $conn = new mysqli($host, $user, $password, $database);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            echo "Error: Unable to connect to MySQL." . PHP_EOL;
+            echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+            echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+            exit;        
+        }    
+        echo "Connected successfully";
+        
+?>
